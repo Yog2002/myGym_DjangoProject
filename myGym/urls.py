bbfb8ca77a1d7ef795adf6871c6ca3f50d222451
Yogesh Chaudhari <yochaudhari2002@gmail.com>
2022-12-17 15:13:52 +0530

    initial commit

--- myGym/urls.py
+++ myGym/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+admin.site.site_header = "GYM Admin"
+admin.site.site_title = "GYM Admin Portal"
+admin.site.index_title = "Welcome to MYGYM Portal"
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),  #manually added  blank path
